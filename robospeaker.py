32c95dd05a769c0924aa3dfa54df90e15fd61ed0
taprenittya-eng <taprenittya@gmail.com>
2026-03-10 21:19:31 +0530

    Add files via upload

--- robospeaker.py
+++ robospeaker.py
@@ -1,6 +1,6 @@
 import pyttsx3
-y=pyttsx3.init()
+engine=pyttsx3.init()
 print("welcome to the robospeaker")
 x=input("what should i speak? ")
-y.say(x)
-y.runandwait()
+engine.say(x)
+engine.runAndWait()
